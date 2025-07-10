dfb62903114d2547b7ae66e4006a335d04a6a240
4dominic888 <4dominin888@gmail.com>
2025-07-09 20:06:00 -0500

    test: :test_tube: update test scope
    Se actualizó el archivo vite.config.ts en los aspectos de rutas de typescript y que acepte test de interfaz de react

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { configDefaults } from 'vitest/config'
+import path from 'path'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'node',
+    environment: 'jsdom',
     exclude: [...configDefaults.exclude],
   },
 })

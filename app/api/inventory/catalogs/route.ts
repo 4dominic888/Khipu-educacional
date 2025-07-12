7912196f2b9b2fd79d059de834c971167a7857b3
4dominic888 <4dominin888@gmail.com>
2025-07-12 16:14:32 -0500

    feat: :sparkles: Adding Next Backend
    Este backend sera necesario por seguridad, ademas se conectaran a los repositorios pronto

---
+++ app/api/inventory/catalogs/route.ts
@@ -0,0 +1,70 @@
+import { rawQueryParamsToSQL, withTransaction } from "@/lib/db";
+import { CatalogItem } from "@/types/khipu/inventory.types";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+    const { searchParams } = new URL(req.url)
+    const raw = searchParams.get('query')
+
+    const { sql, values } = rawQueryParamsToSQL<CatalogItem>({
+        rawQuery: raw,
+        selectFields: 'id, name',
+        tableName: 'catalog_item',
+        defaultQuery: {
+            sort: [{ field: 'name', direction: 'asc' }],
+            pagination: { page: 1, pageSize: 20 }
+        }
+    });
+
+    try {
+        const result = await withTransaction(async (client) => {
+            const { rows } = await client.query<CatalogItem>(sql, values);
+            return NextResponse.json(rows);
+        });
+
+        return NextResponse.json(result);
+    }
+    catch (error) {
+        console.log(error);
+        return NextResponse.json({ error: 'Ocurrió un error inesperado' }, { status: 500 });
+    }
+}
+
+export async function POST(req: Request) {
+    let CatalogItems : CatalogItem[];
+
+    try {
+        const body = await req.json();
+        CatalogItems = Array.isArray(body) ? body : [body];
+    }
+    catch (error) {
+        return NextResponse.json({ error: 'JSON inválido' }, { status: 400 });
+    }
+
+    if (CatalogItems.length === 0) return NextResponse.json({ error: 'Datos no proporcionados' }, { status: 400 });
+
+    const invalid = CatalogItems.find(item => !item.id || !item.name);
+    if (invalid) return NextResponse.json({ error: 'Datos no consistentes' }, { status: 400 });
+
+    const values: string[] = [];
+    const placeholders: string[] = [];
+
+    CatalogItems.forEach(({ id, name }, i) => {
+        const idx = i * 2;
+        placeholders.push(`($${idx + 1}, $${idx + 2})`);
+        values.push(id, name);
+    });
+
+    const query = `INSERT INTO catalog_item (id, name) VALUES ${placeholders.join(', ')}`;
+    try {
+        await withTransaction(async (client) => {
+            const { rowCount } = await client.query(query, values);
+            if (!rowCount) NextResponse.json({ error: 'No se ha agregado ningún elemento' }, { status: 500 });
+            return NextResponse.json({ count: rowCount });
+        });
+    }
+    catch (error) {
+        console.log(error);
+        return NextResponse.json({ error: 'Ocurrió un error inesperado' }, { status: 500 });
+    }
+}
--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,7 @@
 import { Pool, PoolClient } from 'pg';
 import dotenv from 'dotenv';
+import { QueryParams } from '@/types/helpers';
+import { buildOrder, buildPagination, buildWhere } from './utils';
 
 dotenv.config({ quiet: true });
 
@@ -32,4 +34,43 @@ export async function withTransaction<T>(fn: (client: PoolClient) => Promise<T>)
   } finally {
     client.release();
   }
+}
+
+type rawQueryParamsToSQLParams<T> = {
+  rawQuery: string | null,
+  selectFields: string,
+  tableName: string,
+  defaultQuery: QueryParams<T>  
+}
+
+export function rawQueryParamsToSQL<T>({ rawQuery, selectFields = '*', tableName, defaultQuery } : rawQueryParamsToSQLParams<T>) : { sql: string, values: T[] } {
+
+  let query: QueryParams<T> = defaultQuery;
+
+  try {
+      if (rawQuery) {
+          const parsed = JSON.parse(decodeURIComponent(rawQuery));
+          query = {
+              filter: parsed.filter ?? query.filter,
+              sort: parsed.sort ?? query.sort,
+              pagination: parsed.pagination ?? query.pagination
+          }
+      }
+  } catch (error) {
+    throw Error('Parámetro de búsqueda inválido');
+  }
+
+  const where = buildWhere(query.filter);
+  const order = buildOrder(query.sort);
+  const pagination = buildPagination(query.pagination);
+
+  const sqlQuery = `
+      SELECT ${selectFields} FROM ${tableName}
+      ${where.sql}
+      ${order}
+      ${pagination}`
+  ;
+
+  return { sql: sqlQuery, values: [] };
+
 }

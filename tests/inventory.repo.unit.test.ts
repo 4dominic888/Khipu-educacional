3853bfb5a215068e467872205061fe2c3a98fac0
4dominic888 <4dominin888@gmail.com>
2025-07-13 15:28:44 -0500

    feat: :fire: Removing Innecesaries Expects
    Se removio expects innecesarios debido a que el repositorio respectivo ya hace el proceso

--- tests/inventory.repo.unit.test.ts
+++ tests/inventory.repo.unit.test.ts
@@ -76,6 +76,45 @@ describe('inventory logic integration test', () => {
                 sort: [{ field: 'id', direction: 'asc' }],
             });
             expect(result).toStrictEqual(expectedCatalogItems);
+
+
+            const result2 = await catalogItemRepository.getAll({});
+            expect(result2).toBeDefined();
+            expect(result2.length).toBe(3);
+
+            const result3 = await catalogItemRepository.getAll({
+                filter: { name: { op: 'contains', value: 'e' } },
+            });
+            expect(result3.length).toBe(2);
+        });
+    });
+
+    it('should update a catalog item', async () => {
+        await withTestTransaction(async (client) => {
+            const catalogItemRepository = new CatalogInventoryRepository(client);
+            const expectedCatalogItem : CatalogItem = {
+                id: '00000000-0000-0000-0000-000000000001',
+                name: 'Muebles'
+            }
+
+            const result = await catalogItemRepository.update(expectedCatalogItem);
+            expect(result.ok).toBe(true);
+            expect(result.value).toStrictEqual(expectedCatalogItem);
+            expect(result.value.name).toBe('Muebles');
+        });
+    });
+
+    it('should remove a catalog item', async () => {
+        await withTestTransaction(async (client) => {
+            const catalogItemRepository = new CatalogInventoryRepository(client);
+            const result = await catalogItemRepository.remove('00000000-0000-0000-0000-000000000001');
+            expect(result.ok).toBe(true);
+            
+            const getResult = await catalogItemRepository.get('00000000-0000-0000-0000-000000000001');
+            expect(getResult).toBeNull();
+
+            const getAllResult = await catalogItemRepository.getAll();
+            expect(getAllResult.length).toBe(2);
         });
     });
 });

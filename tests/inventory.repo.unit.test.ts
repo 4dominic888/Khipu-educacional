d38f1dd2eae149b75d116a64fa8a820033d5eff6
4dominic888 <4dominin888@gmail.com>
2025-07-12 20:24:53 -0500

    feat: :truck: Rename inventory.int.test to inventory.repo.unit.test
    Se cambio el nombre para indicar que sera una prueba unitaria que pruebe el repositorio

---
+++ tests/inventory.repo.unit.test.ts
@@ -0,0 +1,81 @@
+import { withTestTransaction } from "@/lib/db";
+import { CatalogInventoryRepository } from "@/lib/repositories/inventory";
+import { CatalogItem } from "@/types/khipu/inventory.types";
+
+describe('inventory logic integration test', () => {
+    it('should add a catalog item', async () => {
+        await withTestTransaction(async (client) => {
+            const catalogItemRepository = new CatalogInventoryRepository(client);
+            const catalogItem : CatalogItem = {
+                id: '15454',
+                name: 'PUERTA DE ADOBE'
+            }
+
+            const result = await catalogItemRepository.add(catalogItem);
+            expect(result.ok).toBe(true);
+            expect(result.value).toStrictEqual(catalogItem);
+        });
+    });
+
+    it('should add an amount of catalog items', async () => {
+        await withTestTransaction(async (client) => {
+            const catalogItemRepository = new CatalogInventoryRepository(client);
+            const catalogItems : CatalogItem[] = [
+                {
+                    id: '15454',
+                    name: 'PUERTA DE ADOBE'
+                },
+                {
+                    id: '15455',
+                    name: 'PUERTA DE ADOBE 2'
+                },
+                {
+                    id: '15456',
+                    name: 'PUERTA DE ADOBE 3'
+                }
+            ]
+
+            const result = await catalogItemRepository.addAll(catalogItems);
+            expect(result.ok).toBe(true);
+            expect(result.value).toBeUndefined();
+        });
+    });
+
+    it('should get a catalog item', async () => {
+        await withTestTransaction(async (client) => {
+            const catalogItemRepository = new CatalogInventoryRepository(client);
+            const expectedCatalogItem : CatalogItem = {
+                id: '00000000-0000-0000-0000-000000000001',
+                name: 'Silla'
+            }
+
+            const result = await catalogItemRepository.get(expectedCatalogItem.id);
+            expect(result).toStrictEqual(expectedCatalogItem);
+        });
+    });
+
+    it('should get all catalog items', async () => {
+        await withTestTransaction(async (client) => {
+            const catalogItemRepository = new CatalogInventoryRepository(client);
+            const expectedCatalogItems : CatalogItem[] = [
+                {
+                    id: '00000000-0000-0000-0000-000000000001',
+                    name: 'Silla'
+                },
+                {
+                    id: '00000000-0000-0000-0000-000000000002',
+                    name: 'Mesa'
+                },
+                {
+                    id: '00000000-0000-0000-0000-000000000003',
+                    name: 'Proyector'
+                }
+            ]
+
+            const result = await catalogItemRepository.getAll({
+                sort: [{ field: 'id', direction: 'asc' }],
+            });
+            expect(result).toStrictEqual(expectedCatalogItems);
+        });
+    });
+});
